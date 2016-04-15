49621abb4a733b1132afa75865898068feae7042
agirlprae <willaswt@gmail.com>
2016-04-15 21:40:23 +0700

    #02  SpaceShip
    สร้างยานขึ้นมาโดยยานยังไม่สามารถเคลื่อนไหวได้ตามปุ่มลูกศร

--- Main.java
+++ Main.java
@@ -1,19 +1,23 @@
-import javax.swing.*;
+import java.awt.Color;
 import java.awt.*;
-import java.awt.event.*;
+import javax.swing.JFrame;
+import java.util.ArrayList;
 
 public class Main {
-
 	public static void main(String[] args){
 		JFrame frame = new JFrame("Space War");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(400, 650);
 		frame.getContentPane().setLayout(new BorderLayout());
+		
+		SpaceShip v = new SpaceShip(180, 550, 20, 20);
+		GamePanel gp = new GamePanel();
+		gp.sprites.add(v);
+ 		gp.updateGameUI();
 
-		JPanel gp = new JPanel();
-		gp.setBackground(Color.BLACK);
+ 		frame.getContentPane().setBackground(Color.BLACK);
 		frame.getContentPane().add(gp, BorderLayout.CENTER);
 		frame.setVisible(true);
-	}
 
+	}
 }
